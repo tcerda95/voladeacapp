811577a795709ccbd827dc7f33e2df41ac8ef591
Carla Barruffaldi <cbarruffaldi@itba.edu.ar>
2016-11-17 13:39:35 -0300

    Tabs nuevos

--- app/src/main/java/hci/voladeacapp/Voladeacapp.java
+++ app/src/main/java/hci/voladeacapp/Voladeacapp.java
@@ -5,13 +5,15 @@ import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.app.SearchManager;
 import android.content.Context;
-import android.support.design.widget.BottomNavigationView;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.IdRes;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
 import android.view.Menu;
 import android.view.MenuInflater;
-import android.view.MenuItem;
+
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabSelectListener;
 
 public class Voladeacapp extends AppCompatActivity {
     private Fragment misVuelosFragment;
@@ -25,45 +27,41 @@ public class Voladeacapp extends AppCompatActivity {
 
         final FragmentManager fragmentManager = getFragmentManager();
 
-        final BottomNavigationView bottomNavigationView = (BottomNavigationView)
-                findViewById(R.id.bottom_navigation);
-
         // TODO: preservar el estado de los fragments
 
         misVuelosFragment = new MisVuelosFragment();
         promocionesFragment = new PromocionesFragment();
         resenasFragment = new ResenasFragment();
 
+        BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
+
     /* Comienza en el fragmento Mis Vuelos */
         if (savedInstanceState == null) {
             fragmentManager.beginTransaction().add(R.id.fragment_main_container, misVuelosFragment, MisVuelosFragment.INSTANCE_TAG).commit();
+            bottomBar.selectTabWithId(R.id.action_mis_vuelos);
         }
 
-        bottomNavigationView.setOnNavigationItemSelectedListener(
-                new BottomNavigationView.OnNavigationItemSelectedListener() {
-                    @Override
-                    public boolean onNavigationItemSelected(MenuItem item) {
+        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelected(@IdRes int tabId) {
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+                switch (tabId) {
+                    case R.id.action_mis_vuelos:
+                        setTitle("Mis Vuelos"); // TODO: agarrar los textos de un string resource
+                        transaction.replace(R.id.fragment_main_container, misVuelosFragment, MisVuelosFragment.INSTANCE_TAG).commit();
+                        break;
+                    case R.id.action_promociones:
+                        setTitle("Promociones");
+                        transaction.replace(R.id.fragment_main_container, promocionesFragment, PromocionesFragment.INSTANCE_TAG).commit();
+                        break;
+                    case R.id.action_resenas:
+                        setTitle("Reseñas");
+                        transaction.replace(R.id.fragment_main_container, resenasFragment, ResenasFragment.INSTANCE_TAG).commit();
+                        break;
+                }
 
-                        FragmentTransaction transaction = fragmentManager.beginTransaction();
-
-                        switch (item.getItemId()) {
-                            case R.id.action_mis_vuelos:
-                                setTitle("Mis Vuelos"); // TODO: agarrar los textos de un string resource
-                                transaction.replace(R.id.fragment_main_container, misVuelosFragment, MisVuelosFragment.INSTANCE_TAG).commit();
-                                break;
-                            case R.id.action_promociones:
-                                setTitle("Promociones");
-                                transaction.replace(R.id.fragment_main_container, promocionesFragment, PromocionesFragment.INSTANCE_TAG).commit();
-                                break;
-                            case R.id.action_resenas:
-                                setTitle("Reseñas");
-                                transaction.replace(R.id.fragment_main_container, resenasFragment, ResenasFragment.INSTANCE_TAG).commit();
-                                break;
-                        }
-
-                        return true;
-                    }
-                });
+            }
+        });
     }
 
     @Override
