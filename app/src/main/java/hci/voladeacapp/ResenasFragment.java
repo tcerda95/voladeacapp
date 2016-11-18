9377b88769074036dcf389942559e7d25e4e0111
Carla Barruffaldi <cbarruffaldi@itba.edu.ar>
2016-11-18 18:09:34 -0300

    Inicio de reviews, nada es funcional todavia

--- app/src/main/java/hci/voladeacapp/ResenasFragment.java
+++ app/src/main/java/hci/voladeacapp/ResenasFragment.java
@@ -1,7 +1,9 @@
 package hci.voladeacapp;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,10 +16,21 @@ public class ResenasFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_resenas, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_resenas, container, false);
+
+        FloatingActionButton addButton = (FloatingActionButton)rootView.findViewById(R.id.add_review_button);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), AddReviewActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return rootView;
     }
 }
