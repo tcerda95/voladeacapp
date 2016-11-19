11c3fa26e77da95ab1d9098e7c44e2d887c96b5f
Chelo <mlynch@itba.edu.ar>
2016-11-19 09:33:22 -0300

    BackgroundRefreshReceiver

---
+++ app/src/main/java/hci/voladeacapp/BackgroundRefreshReceiver.java
@@ -0,0 +1,15 @@
+package hci.voladeacapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BackgroundRefreshReceiver extends BroadcastReceiver {
+    public BackgroundRefreshReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
