62c6283d257d7ae7a5d3bdf61cee7f88e7d394ba
Carla Barruffaldi <cbarruffaldi@itba.edu.ar>
2016-11-18 22:23:44 -0300

    Reseñas funcional. Esta horrible el codigo.
    Me acabo de dar cuenta que falta para dejar el comentario

---
+++ app/src/main/java/hci/voladeacapp/PostResenaDummy.java
@@ -0,0 +1,31 @@
+package hci.voladeacapp;
+
+import android.app.AlertDialog;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class PostResenaDummy extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("hola");
+
+        Resena res = (Resena)this.getIntent().getSerializableExtra("resena");
+
+
+        new AlertDialog.Builder(this)
+                .setTitle("Datos de la resena")
+                .setMessage("Numero de vuelo: " + res.getFlightNumber() + "\n" +
+                        "Nombre Aerolinea: " + res.getFlightAirline() + "\n" +
+                        "Amabilidad: " + res.getAmabilidad() + "\n" +
+                        "Confort: " + res.getConfort() + "\n" +
+                        "Comida: " + res.getComida() + "\n" +
+                        "Precio-calidad: " + res.getPreciocalidad() + "\n" +
+                        "Puntualidad: " + res.getPuntualidad() + "\n" +
+                        "Viajero frecuente: " + res.getViajerosFrec() + "\n" +
+                        "Total: " + res.getPuntuacion() + "\n" +
+                        "Recomendado: " + res.getRecomendado() + "\n")
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+    }
+}
