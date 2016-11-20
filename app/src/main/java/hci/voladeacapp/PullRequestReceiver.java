ab6c26e2af819ab89371a76f1d749e2cbd94c69a
Chelo <mlynch@itba.edu.ar>
2016-11-20 14:40:29 -0300

    StorageHelper

--- app/src/main/java/hci/voladeacapp/PullRequestReceiver.java
+++ app/src/main/java/hci/voladeacapp/PullRequestReceiver.java
@@ -13,7 +13,6 @@ import java.util.ArrayList;
 
 import static android.content.Context.MODE_PRIVATE;
 import static hci.voladeacapp.MisVuelosFragment.ACTION_GET_REFRESH;
-import static hci.voladeacapp.MisVuelosFragment.FLIGHT_LIST;
 
 public class PullRequestReceiver extends BroadcastReceiver {
     public PullRequestReceiver() {
@@ -22,20 +21,12 @@ public class PullRequestReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         ArrayList<Flight> flight_details = new ArrayList<>();
-        SharedPreferences sp = context.getSharedPreferences("FLIGHTS", MODE_PRIVATE);
-        String list = sp.getString(FLIGHT_LIST, null); //Si no hay nada devuelve null
 
-        if (list != null) {
-            Gson gson = new Gson();
-            Type type = new TypeToken<ArrayList<Flight>>() {
-            }.getType();
+        flight_details = StorageHelper.getFlights(context.getApplicationContext());
 
-            flight_details = gson.fromJson(list, type);
-
-            for (Flight f : flight_details) {
+        for (Flight f : flight_details) {
                 ApiService.startActionGetFlightStatus(context.getApplicationContext(), f.getAerolinea(), f.getNumber(), ACTION_GET_REFRESH);
             }
         }
 
-    }
 }
