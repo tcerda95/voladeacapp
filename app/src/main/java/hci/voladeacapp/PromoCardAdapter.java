43f0ca6aba9c55a4b2c3f26ddcfd1779aec34155
Luciano Bianchi <lbianchi@itba.edu.ar>
2016-11-24 02:16:43 -0300

    Varios tweaks esteticos. Loaders en promociones

--- app/src/main/java/hci/voladeacapp/PromoCardAdapter.java
+++ app/src/main/java/hci/voladeacapp/PromoCardAdapter.java
@@ -8,9 +8,13 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.PopupMenu;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 
 import java.util.ArrayList;
 import java.util.Map;
@@ -57,7 +61,7 @@ public class PromoCardAdapter extends BaseAdapter {
 
         DealGson deal = (DealGson) getItem(position);
         holder.cityView.setText(deal.city.name.split(",")[0]);
-        holder.priceView.setText("U$D " + deal.price);
+        holder.priceView.setText("U$D " + deal.price.intValue());
 
         // IMAGEN
         setImageView(deal, holder.photoView);
@@ -104,9 +108,24 @@ public class PromoCardAdapter extends BaseAdapter {
     }
 
     private void setImageView(DealGson deal, ImageView imgView) {
+        final ProgressBar progressBar = (ProgressBar) ((View) imgView.getParent()).findViewById(R.id.img_loading_indicator);
+        progressBar.setVisibility(View.VISIBLE);
         if (flightImages.containsKey(deal)) {
             Glide.with(imgView.getContext())
                     .load(flightImages.get(deal))
+                    .listener(new RequestListener<String, GlideDrawable>() {
+                        @Override
+                        public boolean onException(Exception e, String model, Target<GlideDrawable> target, boolean isFirstResource) {
+                            progressBar.setVisibility(View.GONE);
+                            return false;
+                        }
+
+                        @Override
+                        public boolean onResourceReady(GlideDrawable resource, String model, Target<GlideDrawable> target, boolean isFromMemoryCache, boolean isFirstResource) {
+                            progressBar.setVisibility(View.GONE);
+                            return false;
+                        }
+                    })
                     .centerCrop()
                     .crossFade()
                     .into(imgView);
