0dc6a3a00799c9fc369ef9b736e6d26ca3c8294a
tcerdaITBA <tcerda@itba.edu.ar>
2016-11-27 21:38:15 -0300

    Resuelto conflicto en strings y UP de FlightDetails regresa a PromoDetails

--- app/src/main/java/hci/voladeacapp/Voladeacapp.java
+++ app/src/main/java/hci/voladeacapp/Voladeacapp.java
@@ -44,6 +44,8 @@ public class Voladeacapp extends AppCompatActivity {
 
     private static final String TAB_ID = "hci.voladeacapp.Voladeacapp.TAB_ID";
 
+    public static final String PARENTSHIP = "hci.voladeacapp.Voladeacapp.PARENTSHIP";
+
     public final static int LOCATION_PERMISSION_REQUEST_CODE = 1;
     public static int CAMERA_PERMISSION_REQUEST_CODE = 2;
 
